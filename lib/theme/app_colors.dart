7190f960b23677838a3585d32f5c4f995e1acfc3
Ekojode Oma-Victor <ekojodeoma@gmail.com>
2023-10-16 08:03:35 +0100

    Set up color theme

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const bgColor = Color(0XFFECECEE);
+  static const primaryColor = Color(0XFF76984B);
+  static const altPrimary = Color(0XFF67864A);
+  static const white = Colors.white;
+  static const black = Color(0XFF222222);
+}
